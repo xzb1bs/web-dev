2cd439b73926b86c9de70154694dd8fe6574fa54
xzb1bs <beibarssagidolla47@gmail.com>
2025-02-10 09:16:13 +0500

    lab 3

--- Lab 3/To Do/script.js
+++ Lab 3/To Do/script.js
@@ -1,19 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const taskInput = document.getElementById("taskInput");
     const taskList = document.getElementById("taskList");
-    const addTaskBtn = document.getElementById("addTaskBtn");
+    const addTaskBtn = document.getElementById("addTaskBtn"); 
 
     function addTask() {
-        if (!taskInput.value.trim()) return;
+        const taskText = taskInput.value.trim();
+        if (taskText === "") return; 
         const li = document.createElement("li");
-        li.innerHTML = `
-        <input type="checkbox">
-        <span>${taskInput.value}</span>
-        <span class="delete">
-            <img src="https://www.flaticon.com/free-icons/delete" alt="Delete" width="20">
-        </span>
-    `;
-            taskList.appendChild(li);
+        li.innerHTML = `<input type="checkbox"> <span>${taskInput.value}</span> <span class="delete">🗑</span>`;
+        taskList.appendChild(li);
         taskInput.value = "";
         saveTasks();
     }
