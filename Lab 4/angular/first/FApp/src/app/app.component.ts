4a613e93ef747cb8ac478acf1e273e14072577a7
xzb1bs <beibarssagidolla47@gmail.com>
2025-02-13 07:47:02 +0500

    lab

--- Lab 4/angular/first/FApp/src/app/app.component.ts
+++ Lab 4/angular/first/FApp/src/app/app.component.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'FApp';
+  title = 'angular-app';
+  tooltip = `This is a tooltip`;
 }
